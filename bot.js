3352c8b3f4ca5ffdf0038572da4ce57075a8c5b7
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-10-29 16:19:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,7 @@ const { getPlayerInfo } = require('noblox.js');
 const bot = new discord.Client();
 
 bot.on("ready", () => {
-    bot.user.setPresence({
-        game: {
-            name: "Watching over you.",
-            type: "STREAMING"
-        }
-    });
+    
 });
 
 bot.login(process.env.BOT_TOKEN);
