97ec24dbb8b2a3210897313909a8cf9977db7729
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-10-29 16:08:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,7 @@ const bot = new discord.Client();
 const rNames = [Synceratus, RedactedZero, Taysav123, R0bustic, CreepySins, lxuca, unix_system];
 
 bot.on("ready", () => {
-
-    while(true) {
-        for(let i = 0; i < rNames.length; i++) {
-            let playerInfo = await getPlayerInfo(String(rNames[i]))
-        }
-    }
+    
 });
 
 bot.login(process.env.BOT_TOKEN);
