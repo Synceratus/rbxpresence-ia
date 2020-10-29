31ecdaafbf5bf6f5ac49c4b746dd966fc1fd8410
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-10-29 19:08:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on("ready", () => {
+    client.user.setStatus("online");
+    client.user.setPresence({
+        game: {
+            name: "test",
+            type: "WATCHING"
+        }
+    });
 });
 
 client.login(process.env.BOT_TOKEN);
