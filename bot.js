643808f3c43757c44a5ba1458f173ab4a2382532
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-10-29 17:54:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,7 @@
 const Discord = require('discord.js');
-const { getPlayerInfo } = require('noblox.js');
 const Bot = new Discord.Client();
-const Names = ["Synceratus", "RedactedZero", "Taysav123", "R0bustic", "CreepySins", "lxuca", "unix_system"];
 
 bot.on("ready", () => {
-    
 });
-
-while (true) {
-    for (let i=0; i<Names.length; i++) {
-        let plrInfo = await getPlayerInfo(String(Names[i]));
-    }
-}
 
 bot.login(process.env.BOT_TOKEN);
