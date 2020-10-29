92e34d3dc5a6c57234e4447e15c91df941b35173
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-10-29 15:53:26 +0100

    Get Presence

--- bot.js
+++ bot.js
@@ -1,4 +1,14 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const discord = require('discord.js');
+const { getPlayerInfo } = require('noblox.js');
+const bot = new discord.Client();
+const rNames = [Synceratus, RedactedZero, Taysav123, R0bustic, CreepySins, lxuca, unix_system];
+
+bot.on("ready", () => {
+    while(true) {
+        for(let i = 0; i < rNames.length; i++) {
+            let playerInfo = await getPlayerInfo(String(rNames[i]))
+        }
+    }
+});
 
 bot.login(process.env.BOT_TOKEN);
