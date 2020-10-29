71ab06d3feb77392ae1a54c6b2ace8750cb2d16d
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-10-29 16:02:51 +0100

    Status update.

--- bot.js
+++ bot.js
@@ -4,6 +4,14 @@ const bot = new discord.Client();
 const rNames = [Synceratus, RedactedZero, Taysav123, R0bustic, CreepySins, lxuca, unix_system];
 
 bot.on("ready", () => {
+
+    bot.user.setPresence({
+        game: {
+            name: "Watching over you.",
+            type: "STREAMING"
+        }
+    });
+
     while(true) {
         for(let i = 0; i < rNames.length; i++) {
             let playerInfo = await getPlayerInfo(String(rNames[i]))
