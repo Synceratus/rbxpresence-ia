894c32199a12be97b4beac3db38f2ed6594d67d8
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-10-29 16:12:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,8 @@
 const discord = require('discord.js');
 const { getPlayerInfo } = require('noblox.js');
 const bot = new discord.Client();
-const rNames = [Synceratus, RedactedZero, Taysav123, R0bustic, CreepySins, lxuca, unix_system];
 
 bot.on("ready", () => {
-
     bot.user.setPresence({
         game: {
             name: "Watching over you.",
