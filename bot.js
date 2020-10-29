dd326fb85d2d29a1f9669c79ffcd3dd69cfed3ca
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-10-29 16:10:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,13 @@ const bot = new discord.Client();
 const rNames = [Synceratus, RedactedZero, Taysav123, R0bustic, CreepySins, lxuca, unix_system];
 
 bot.on("ready", () => {
-    
+
+    bot.user.setPresence({
+        game: {
+            name: "Watching over you.",
+            type: "STREAMING"
+        }
+    });
 });
 
 bot.login(process.env.BOT_TOKEN);
