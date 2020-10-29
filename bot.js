612d0b90700c538601e491e5ec36662b9b111925
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-10-29 19:43:08 +0100

    Update

--- bot.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on("ready", () => {
-    client.user.setActivity('over you.', { type: "WATCHING"});
-    client.user.setStatus('online');
-});
-
-client.login(process.env.BOT_TOKEN);
