0da6d877c949b1c281a33d5c632b68003dcf4c02
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-11-04 22:45:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,8 @@ const client = new Discord.Client();
 const Names = ["Synceratus", "RedactedZero", "Taysav123", "R0bustic", "CreepySins", "lxuca", "unix_system", "Reincracy", "knoflikCZ", "dragon1warrior"];
 
 var debounce = false
+var logChannelName = "presence_logs"
+
 
 dotenv.config();
 
@@ -27,17 +29,29 @@ function wait(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
+var onlinePeeps = []
+
 async function getInfos() {
         while (true) {
             for (let i = 0; i < Names.length; i++) {
                 let userIds = await noblox.getIdFromUsername(String(Names[i]));
                 if (userIds != null) {
                     let plrInfo = await noblox.getPresences([Number(userIds)]);
-                    console.log(plrInfo.userPresences);
+                    const result = onlinePeeps.find(element => element == Names[i])
+                    const index = onlinePeeps.indexOf(Names[i])
+                    if (plrInfo["userPresences"][0]["userPresenceType"] == 2 && plrInfo["userPresences"][0]["lastLocation"] == "[SCP] Area 27v2" && result == undefined) {
+                      onlinePeeps[onlinePeeps.length] = Names[i];
+                      const channel = client.channels.cache.find(channel => channel.name === logChannelName)
+                      channel.send(Names[i] + " is online.")
+                    } else if (result != undefined && plrInfo["userPresences"][0]["userPresenceType"] != 2) {
+                      onlinePeeps.splice(index, 1)
+                      const channel = client.channels.cache.find(channel => channel.name === logChannelName)
+                      channel.send(Names[i] + " is offline.")
+                    }
                     await wait(5000);
                 }
             }
-        await wait(600000);
+          await wait(600000);
         }
 }
 
