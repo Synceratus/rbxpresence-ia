9235e9c7373fdacde665d319b4da5b63d54c5b30
Synceratus <65905477+Synceratus@users.noreply.github.com>
2020-11-04 20:21:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ async function getInfos() {
                 let userIds = await noblox.getIdFromUsername(String(Names[i]));
                 if (userIds != null) {
                     let plrInfo = await noblox.getPresences([Number(userIds)]);
-                    console.log(plrInfo);
+                    console.log(plrInfo.userPresences["placeId"]);
                     await wait(5000);
                 }
             }
